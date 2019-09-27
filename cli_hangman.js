9682f8532a831fe79cfea5e390f2d4645fbeec7f
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-27 15:44:00 -0700

    created functions to read from file, pick a random word, create letter objects to mask/unmask for guessing

--- cli_hangman.js
+++ cli_hangman.js
@@ -1,3 +1,53 @@
 // node requires
 var fs = require("fs");
 var inquirer = require("inquirer");
+
+// read from words.txt and generate an array of words to choose from
+function getWords() {
+    fs.readFile("./words.txt", "utf8", function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            var wordArray = data.split("\r\n");
+            // do something after creating wordArray
+            pickWord(wordArray);
+        }
+    })
+}
+
+// pick a random word from the array
+function pickWord(array) {
+    var index = Math.floor(Math.random() * array.length) + 1;
+    var word = array[index];
+    // build letters and push into an array
+    var wordArray = [];
+    for (var i = 0; i < word.length; i++) {
+        var letter = new Letter(word[i]);
+        wordArray.push(letter);
+    }
+    // do something from here
+    printWord(wordArray);
+}
+
+// constructor to hide, mask letters
+function Letter(letter) {
+    this.letter = letter;
+    this.guessed = false;
+    this.printLetter = function() {
+        if (this.guessed) {
+            return this.letter;
+        } else {
+            return "_";
+        }
+    }
+}
+
+// call the printLetter function from array
+function printWord(array) {
+    var string = "";
+    array.forEach(function(letter) {
+        string += letter.printLetter() + " ";
+    })
+    console.log(string);
+}
+getWords();
