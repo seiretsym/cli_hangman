9195d232c953d477cf8f34d2c5c607f4ba0694f5
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-27 23:52:46 -0700

    add functions for correct/wrong guesses and win/lose situations

--- cli_hangman.js
+++ cli_hangman.js
@@ -3,8 +3,11 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 var Letter = require("./letter.js");
 var Word = require("./word.js");
-var guessedLetters = "a";
+
+// global variables
+var guessedLetters = "";
 var correctLetters = 0;
+var remainingGuesses = 10;
 
 // read from words.txt and generate an array of words to choose from
 function getWords() {
@@ -36,58 +39,98 @@ function pickWord(array) {
 
 // let's play!
 function playGame(word) {
-    // spacer
-    console.log("\n\n");
-    // print word
-    word.printWord();
+    // check if there are any remaining guesses left
+    if (remainingGuesses > 0) {
+        // spacer
+        console.log("\n\n");
+        // print word
+        word.printWord();
 
-    console.log("\n");
+        console.log("\nRemaining Guesses: " + remainingGuesses + "\n");
 
-    // prompt user for input
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Guess a letter:",
-            name: "guess",
-            validate: function(input) {
-                // create string to check character is alphabet
-                var alpha = "abcdefghijklmnopqrstuvwxyz";
+        // prompt user for input
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Guess a letter:",
+                name: "guess",
+                validate: function(input) {
+                    // create string to check character is alphabet
+                    var alpha = "abcdefghijklmnopqrstuvwxyz";
 
-                // check if input is only 1 char
-                if (input.length !== 1) {
-                    return "One letter at a time!";
-                }
-                // check if input is alphabet
-                else if (alpha.indexOf(input.toLowerCase()) === -1) {
-                    return "That's not an alphabet!";
-                }
-                // check if letter has been guessed
-                else if (guessedLetters.indexOf(input.toLowerCase()) !== -1) {
-                    return "You've already guessed that letter!";
-                } else {
-                    return true;
+                    // check if input is only 1 char
+                    if (input.length !== 1) {
+                        return "One letter at a time!";
+                    }
+                    // check if input is alphabet
+                    else if (alpha.indexOf(input.toLowerCase()) === -1) {
+                        return "That's not an alphabet!";
+                    }
+                    // check if letter has been guessed
+                    else if (guessedLetters.indexOf(input.toLowerCase()) !== -1) {
+                        return "You've already guessed that letter!";
+                    } else {
+                        return true;
+                    }
                 }
             }
-        }
-    ]).then(function(response) {
-        // run word.guess() to check all letters if there's a match
-        word.guess(response.guess);
-        // add letter to guessedLetters
-        guessedLetters += response.guess.toLowerCase();
-        
-        // check if all letters are guessed
-        if (correctLetters === word.letters.length) {
-            // run win function
-            win();
-        }
-        else if (correctLetters === word.correctLetters) {
-            // run correctGuess function
-            correctGuess();
-        } else {
-            // run wrongGuess function
-            wrongGuess();
-        }
-    })
+        ]).then(function(response) {
+            // convert response input to lowercase
+            var letter = response.guess.toLowerCase();
+            // run word.guess() to check all letters if there's a match
+            word.guess(response.guess);
+            // add letter to guessedLetters
+            guessedLetters += letter;
+            
+            // check if all letters are guessed
+            if (correctLetters === word.letters.length) {
+                // run win function
+                winGame(word);
+            }
+            else if (correctLetters === word.correctLetters) {
+                // run correctGuess function
+                correctGuess(letter);
+            } else {
+                // run wrongGuess function
+                wrongGuess(letter);
+            }
+        })
+    } else {
+        // you lose
+        loseGame(word);
+    }
+}
+
+// letting you know you're doing something right
+function correctGuess(letter) {
+    console.log("\n" + letter + " was found in the word!")
+    // guess again by looping playGame()
+    playGame();
+}
+
+// letting you know you're doing something wrong
+function wrongGuess(letter) {
+    console.log("\n" + letter + " was not found in the word!")
+    // deduct remaining guesses
+    remainingGuesses--;
+    // guess again by looping playGame()
+    playGame();
+}
+
+// you won!
+function winGame(word) {
+    // run word.win() function
+    word.win();
+    // play again?
+    playAgain();
+}
+
+// you lost :(
+function loseGame(word) {
+    // run word.lose
+    word.lose();
+    // play again?
+    playAgain();
 }
 
 getWords();
