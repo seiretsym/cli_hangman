57c4e24732ef1deedea2ff22375d88fe4cecafdf
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-27 23:41:47 -0700

    add function to prompt user input for guessing letters

--- cli_hangman.js
+++ cli_hangman.js
@@ -3,6 +3,8 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 var Letter = require("./letter.js");
 var Word = require("./word.js");
+var guessedLetters = "a";
+var correctLetters = 0;
 
 // read from words.txt and generate an array of words to choose from
 function getWords() {
@@ -29,9 +31,63 @@ function pickWord(array) {
     }
     // push the letters into a word construct
     var word = new Word(wordArray);
+    playGame(word);
+}
+
+// let's play!
+function playGame(word) {
+    // spacer
+    console.log("\n\n");
+    // print word
     word.printWord();
-    word.guess("a");
-    word.printWord();
+
+    console.log("\n");
+
+    // prompt user for input
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Guess a letter:",
+            name: "guess",
+            validate: function(input) {
+                // create string to check character is alphabet
+                var alpha = "abcdefghijklmnopqrstuvwxyz";
+
+                // check if input is only 1 char
+                if (input.length !== 1) {
+                    return "One letter at a time!";
+                }
+                // check if input is alphabet
+                else if (alpha.indexOf(input.toLowerCase()) === -1) {
+                    return "That's not an alphabet!";
+                }
+                // check if letter has been guessed
+                else if (guessedLetters.indexOf(input.toLowerCase()) !== -1) {
+                    return "You've already guessed that letter!";
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(response) {
+        // run word.guess() to check all letters if there's a match
+        word.guess(response.guess);
+        // add letter to guessedLetters
+        guessedLetters += response.guess.toLowerCase();
+        
+        // check if all letters are guessed
+        if (correctLetters === word.letters.length) {
+            // run win function
+            win();
+        }
+        else if (correctLetters === word.correctLetters) {
+            // run correctGuess function
+            correctGuess();
+        } else {
+            // run wrongGuess function
+            wrongGuess();
+        }
+    })
 }
 
 getWords();
